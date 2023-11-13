362a8d0e9c682ae41793efee0cbdcd7691806c3b
hasenradball <mail@frankhaefele.de>
2023-11-13 12:14:16 +0100

    first commit

---
+++ examples/ConnectWithWPA/ConnectWithWPA.ino
@@ -0,0 +1,110 @@
+/*
+ This example connects to an WPA encrypted WiFi network.
+ Then it prints the MAC address of the WiFi module,
+ the IP address obtained, and other network details.
+
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 13 Nov 2023
+ by Frank Häfele
+ */
+#include <SPI.h>
+#include <NINA-WiFi.h>
+
+#include "arduino_secrets.h" 
+// ==> please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;    // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  WiFi.setTimeout(0);
+  // use non blocking begin
+  WiFi.begin(ssid, pass);
+  // attempt to connect to WiFi network:
+  Serial.print("Attempting to connect to open SSID: ");
+  Serial.println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(". ");
+    delay(500);
+  }
+  if (WiFi.status() == WL_CONNECTED) {
+    // you're connected now, so print out the data:
+    Serial.print("You're connected to the network");
+    printCurrentNet();
+    printWifiData();
+  }
+}
+
+void loop() {
+  // check the network connection once every 10 seconds:
+  delay(10000);
+  printCurrentNet();
+}
+
+void printWifiData() {
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("RSSI: ");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type: ");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printMacAddress(byte mac[]) {
+  for (int i = 5; i >= 0; i--) {
+    if (mac[i] < 16) {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0) {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
+}
---
+++ src/NINA-Wi-Fi.h
@@ -0,0 +1,10 @@
+/*
+  This file is part of the NINA-Wi-Fi library.
+*/
+
+#ifndef _NINA_WI_FI_H_
+#define _NINA_WI_FI_H_
+
+#include "WiFi.h"
+
+#endif
